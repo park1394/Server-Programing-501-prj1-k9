b2044e64dec6fb1531824699bf20e9ab85036d88
lsy3709 <lsy3709@naver.com>
2026-02-12 14:58:44 +0900

    6교시, @Repository 2가지 구현한 클래스를 구분하는 방법 예시

--- src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_2_SampleService_0212.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_2_SampleService_0212.java
@@ -2,6 +2,7 @@ package com.busanit501.jsp_server_project1.springex_0212.sample;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service // 시스템에 등록. 어떤용도, 이 클래스가 서비스로 사용된다고,
@@ -9,5 +10,7 @@ import org.springframework.stereotype.Service;
 @ToString
 @RequiredArgsConstructor
 public class _2_SampleService_0212 {
+
+    @Qualifier("normal")
     private final _1_SampleDAO_0212 sampleDAO0212;
 }
